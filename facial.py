923d8fbb560de65e2bba31d6c3c9112d37c399c6
Synno <sean.x.han@berkeley.edu>
2014-02-16 08:08:34 -0800

    Added antigray

--- facial.py
+++ facial.py
@@ -1,27 +1,37 @@
 import numpy as np
 import cv2
 
-face_cascades = []
-face_cascades.append(cv2.CascadeClassifier('/opt/local/share/OpenCV/haarcascades/haarcascade_frontalface_default.xml'))
-face_cascades.append(cv2.CascadeClassifier('/opt/local/share/OpenCV/haarcascades/haarcascade_frontalface_alt.xml'))
-face_cascades.append(cv2.CascadeClassifier('/opt/local/share/OpenCV/haarcascades/haarcascade_frontalface_alt_tree.xml'))
-face_cascades.append(cv2.CascadeClassifier('/opt/local/share/OpenCV/haarcascades/haarcascade_frontalface_alt2.xml'))
-eye_cascades = []
-eye_cascades.append(cv2.CascadeClassifier('/opt/local/share/OpenCV/haarcascades/haarcascade_eye.xml'))
-eye_cascades.append(cv2.CascadeClassifier('/opt/local/share/OpenCV/haarcascades/haarcascade_eye_tree_eyeglasses.xml'))
+def findFaces(picFile):
+  face_cascades = []
+  face_cascades.append(cv2.CascadeClassifier('/usr/local/share/OpenCV/haarcascades/haarcascade_frontalface_default.xml'))
+  face_cascades.append(cv2.CascadeClassifier('/usr/local/share/OpenCV/haarcascades/haarcascade_frontalface_alt.xml'))
+  face_cascades.append(cv2.CascadeClassifier('/usr/local/share/OpenCV/haarcascades/haarcascade_frontalface_alt_tree.xml'))
+  face_cascades.append(cv2.CascadeClassifier('/usr/local/share/OpenCV/haarcascades/haarcascade_frontalface_alt2.xml'))
+  eye_cascades = []
+  eye_cascades.append(cv2.CascadeClassifier('/usr/local/share/OpenCV/haarcascades/haarcascade_eye.xml'))
+  eye_cascades.append(cv2.CascadeClassifier('/usr/local/share/OpenCV/haarcascades/haarcascade_eye_tree_eyeglasses.xml'))
 
-img = cv2.imread('mirror11.jpg')
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+  img = cv2.imread(picFile)
+  gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+  antigray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+  for i in xrange(gray.shape[0]):
+    for j in xrange(gray.shape[1]):
+      antigray[i][j] = 255 - gray[i][j]
 
-faces = []
-for face_cascade in face_cascades:
-  faces.append(face_cascade.detectMultiScale(gray, 1.3, 5))
-for stuff in faces:
-  for (x,y,w,h) in stuff:
-    cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-    roi_gray = gray[y:y+h, x:x+w]
-    roi_color = img[y:y+h, x:x+w]
+  faces = []
+  for face_cascade in face_cascades:
+    faces.append(face_cascade.detectMultiScale(gray, 1.13, 10))
+    faces.append(face_cascade.detectMultiScale(antigray, 1.13, 10))
+  for stuff in faces:
+    for (x,y,w,h) in stuff:
+      cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+      roi_gray = gray[y:y+h, x:x+w]
+      roi_color = img[y:y+h, x:x+w]
 
-cv2.imshow('img',img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+  cv2.imshow('img',img)
+  cv2.waitKey(0)
+  cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+  import sys
+  findFaces(sys.argv[1])
