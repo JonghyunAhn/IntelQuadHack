3a37f632d729d3951f6172f67ccc36010d69d972
Synno <sean.x.han@berkeley.edu>
2014-02-16 08:09:49 -0800

    Added antigray with opt

--- facial.py
+++ facial.py
@@ -3,13 +3,13 @@ import cv2
 
 def findFaces(picFile):
   face_cascades = []
-  face_cascades.append(cv2.CascadeClassifier('/usr/local/share/OpenCV/haarcascades/haarcascade_frontalface_default.xml'))
-  face_cascades.append(cv2.CascadeClassifier('/usr/local/share/OpenCV/haarcascades/haarcascade_frontalface_alt.xml'))
-  face_cascades.append(cv2.CascadeClassifier('/usr/local/share/OpenCV/haarcascades/haarcascade_frontalface_alt_tree.xml'))
-  face_cascades.append(cv2.CascadeClassifier('/usr/local/share/OpenCV/haarcascades/haarcascade_frontalface_alt2.xml'))
+  face_cascades.append(cv2.CascadeClassifier('/opt/local/share/OpenCV/haarcascades/haarcascade_frontalface_default.xml'))
+  face_cascades.append(cv2.CascadeClassifier('/opt/local/share/OpenCV/haarcascades/haarcascade_frontalface_alt.xml'))
+  face_cascades.append(cv2.CascadeClassifier('/opt/local/share/OpenCV/haarcascades/haarcascade_frontalface_alt_tree.xml'))
+  face_cascades.append(cv2.CascadeClassifier('/opt/local/share/OpenCV/haarcascades/haarcascade_frontalface_alt2.xml'))
   eye_cascades = []
-  eye_cascades.append(cv2.CascadeClassifier('/usr/local/share/OpenCV/haarcascades/haarcascade_eye.xml'))
-  eye_cascades.append(cv2.CascadeClassifier('/usr/local/share/OpenCV/haarcascades/haarcascade_eye_tree_eyeglasses.xml'))
+  eye_cascades.append(cv2.CascadeClassifier('/opt/local/share/OpenCV/haarcascades/haarcascade_eye.xml'))
+  eye_cascades.append(cv2.CascadeClassifier('/opt/local/share/OpenCV/haarcascades/haarcascade_eye_tree_eyeglasses.xml'))
 
   img = cv2.imread(picFile)
   gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
