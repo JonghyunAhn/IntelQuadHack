fb14928bcd3921e08aacaa6786516988aa275f4b
Leo Wu <leowu@berkeley.edu>
2014-02-16 08:15:44 -0800

    added facial recog shit

--- facial.py
+++ facial.py
@@ -1,28 +1,48 @@
 import numpy as np
 import cv2
 
-face_cascades = []
-face_cascades.append(cv2.CascadeClassifier('/usr/local/share/OpenCV/haarcascades/haarcascade_frontalface_default.xml'))
-face_cascades.append(cv2.CascadeClassifier('/usr/local/share/OpenCV/haarcascades/haarcascade_frontalface_alt.xml'))
-face_cascades.append(cv2.CascadeClassifier('/usr/local/share/OpenCV/haarcascades/haarcascade_frontalface_alt_tree.xml'))
-face_cascades.append(cv2.CascadeClassifier('/usr/local/share/OpenCV/haarcascades/haarcascade_frontalface_alt2.xml'))
-face_cascades.append(cv2.CascadeClassifier('/usr/local/share/OpenCV/haarcascades/haarcascade_fullbody.xml'))
-face_cascades.append(cv2.CascadeClassifier('/usr/local/share/OpenCV/haarcascades/haarcascade_lowerbody.xml'))
-face_cascades.append(cv2.CascadeClassifier('/usr/local/share/OpenCV/haarcascades/haarcascade_upperbody.xml'))
-face_cascades.append(cv2.CascadeClassifier('/usr/local/share/OpenCV/haarcascades/haarcascade_profileface.xml'))
+def findFaces(picFile):
+  face_cascades = []
+  face_cascades.append(cv2.CascadeClassifier('/usr/local/share/OpenCV/haarcascades/haarcascade_frontalface_default.xml'))
+  face_cascades.append(cv2.CascadeClassifier('/usr/local/share/OpenCV/haarcascades/haarcascade_frontalface_alt.xml'))
+  face_cascades.append(cv2.CascadeClassifier('/usr/local/share/OpenCV/haarcascades/haarcascade_frontalface_alt_tree.xml'))
+  face_cascades.append(cv2.CascadeClassifier('/usr/local/share/OpenCV/haarcascades/haarcascade_frontalface_alt2.xml'))
+  face_cascades.append(cv2.CascadeClassifier('/usr/local/share/OpenCV/haarcascades/haarcascade_profileface.xml'))
+  
+  body_cascades = []
+  body_cascades.append(cv2.CascadeClassifier('/usr/local/share/OpenCV/haarcascades/haarcascade_fullbody.xml'))
+  body_cascades.append(cv2.CascadeClassifier('/usr/local/share/OpenCV/haarcascades/haarcascade_lowerbody.xml'))
+  body_cascades.append(cv2.CascadeClassifier('/usr/local/share/OpenCV/haarcascades/haarcascade_upperbody.xml'))
+  
+  img = cv2.imread(picFile)
+  gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+  antigray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+  for i in xrange(gray.shape[0]):
+    for j in xrange(gray.shape[1]):
+      antigray[i][j] = 255-gray[i][j]
+  
+  faces = []
+  for face_cascade in face_cascades:
+    faces.append(face_cascade.detectMultiScale(gray, 1.13, 10))
+    faces.append(face_cascade.detectMultiScale(antigray, 1.13, 10))
+  bodies = []
+  for body_cascade in body_cascades:
+    bodies.append(body_cascade.detectMultiScale(gray, 1.08, 10))
+    bodies.append(body_cascade.detectMultiScale(antigray, 1.08, 10))
+  for stuff in faces:
+    for (x,y,w,h) in stuff:
+      cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+      roi_gray = gray[y:y+h, x:x+w]
+      roi_color = img[y:y+h, x:x+w]
+  
+  for stuff in bodies:
+    for (x,y,w,h) in stuff:
+      cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+      roi_gray = gray[y:y+h, x:x+w]
+      roi_color = img[y:y+h, x:x+w]
 
-img = cv2.imread('Chinese_Crowd.jpg')
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-faces = []
-for face_cascade in face_cascades:
-  faces.append(face_cascade.detectMultiScale(gray, 1.3, 5))
-for stuff in faces:
-  for (x,y,w,h) in stuff:
-    cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-    roi_gray = gray[y:y+h, x:x+w]
-    roi_color = img[y:y+h, x:x+w]
-
-cv2.imshow('img',img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+  return img
+  
+if __name__ == "__main__":
+  import sys
+  findFaces(sys.argv[1])
